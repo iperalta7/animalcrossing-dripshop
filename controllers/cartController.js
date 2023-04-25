3c9c3ec14964865ba3a4fc9c8152302de563bc18
Isaiah Peralta <iperalta@uri.edu>
2023-04-25 00:59:55 -0400

    UPDATE:  cartController to empty cart page for now

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,49 +6,9 @@ const router = express.Router();
 const User = require("../models/User");
 
 // Show the login page
-router.get("/login", (req, res) => {
-    res.render("login");
+router.get("/cart", (req, res) => {
+    res.render("cart");
 });
 
-// Handle login
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-
-        if (!user) {
-            req.session.error_msg = "Email not found";
-            return res.redirect("/auth/login");
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (isMatch) {
-            req.session.user = {
-                id: user.id,
-                email: user.email,
-            };
-            req.session.success_msg = "Logged in successfully";
-            res.redirect("/shop");
-        } else {
-            req.session.error_msg = "Incorrect password";
-            res.redirect("/auth/login");
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Server error");
-    }
-});
-
-// Handle logout
-router.get("/logout", (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).send("Server error");
-        }
-        res.clearCookie("connect.sid");
-        res.redirect("/auth/login");
-    });
-});
 
 module.exports = router;
