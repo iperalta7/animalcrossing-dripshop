fd5e60dadbf5b76c8d30e8ab91fc480091bc5b7a
Isaiah Peralta <iperalta@uri.edu>
2023-04-27 02:55:57 -0400

    ADDED: display orders and create new orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,15 +1,48 @@
 const express = require("express");
 const router = express.Router();
 
+const Order = require("../models/Order.js");
 
-// Show the orders page
-router.get("/orders", (req, res) => {
-    let user = req.session.username;
-    res.render("orders", 
-    {title: "Orders Page",
-    page_title:`${user}'s Orders`,
-    isLoggedIn: req.session.isLoggedIn}
-    );
-});
+
+// Add a new route for fetching and displaying orders
+router.get('/orders', (req, res) => {
+    const custID = req.session.username;
+  
+    Order.getOrdersByCustomer(custID, (error, orders) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error fetching orders.');
+      } else {
+        res.render('orders', {
+            title: `${req.session.username}'s Cart`,
+          page_title: `${req.session.username}'s Cart`,
+          isLoggedIn: req.session.isLoggedIn, 
+            orders: orders });
+      }
+    });
+  });
+
+// Add a new route for order creation
+router.post('/order/create', (req, res) => {
+    const custID = req.session.username;
+    const fitIDs = req.body.fitIDs;
+    console.log(req.body.fitIDs);
+  
+    Order.createOrder(custID, fitIDs, (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error creating order.');
+      } else {
+        const Cart = require("../models/Cart.js");
+
+        Cart.deleteCartByCustomer(custID, (error, result) => {
+          if (error) {
+            console.error(error);
+          }
+        });
+        res.redirect('/orders'); // Redirect to a success page, or any other page you'd like.
+      }
+    });
+  });
 
 module.exports = router;
---
+++ models/Order.js
@@ -0,0 +1,54 @@
+const connection = require("../database.js");
+
+class Order {
+  static getOrdersByCustomer(custID, callback) {
+    connection.query(
+      `SELECT o.orderID,GROUP_CONCAT(oi.fitID) AS fitIDs
+       FROM \`order\` o
+       LEFT JOIN order_items oi ON o.orderID = oi.orderID
+       WHERE o.custID = ?
+       GROUP BY o.orderID`,
+      [custID],
+      (error, results) => {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, results);
+        }
+      }
+    );
+  }
+
+  static createOrder(custID, fitIDs, callback) {
+    // Insert a new row into the order table with the custID
+    connection.query(
+      "INSERT INTO `order` (`custID`) VALUES (?)",
+      [custID],
+      (error, result) => {
+        if (error) {
+          callback(error, null);
+        } else {
+          // Get the orderID of the newly inserted row
+          const orderID = result.insertId;
+
+          // Loop through the array of fitIDs and insert a new row into the order_items table for each fitID
+          for (let i = 0; i < fitIDs.length; i++) {
+            connection.query(
+              "INSERT INTO `order_items` (`orderID`, `fitID`) VALUES (?, ?)",
+              [orderID, fitIDs[i]],
+              (error, result) => {
+                if (error) {
+                  callback(error, null);
+                }
+              }
+            );
+          }
+
+          callback(null, "Order created successfully.");
+        }
+      }
+    );
+  }
+}
+
+module.exports = Order;
