83c382fdecb00a92c571e8c24b4d56aaab99682c
Isaiah Peralta <iperalta@uri.edu>
2023-04-27 12:13:38 -0400

    FIX: use pool for production

--- database.js
+++ database.js
@@ -3,16 +3,19 @@ const mysql = require('mysql2');
 
 
 //create the connection the mysql server (LOCAL ONLY)
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
   });
   
   
   // test mysql database connection
-  connection.connect((err) => {
+  connection.getConnection((err) => {
     if (err) throw err;
     console.log('Connected to database!');
   });
