3830c6a2de85fe07a14bea4ac660468d4fc07abf
Isaiah Peralta <iperalta@uri.edu>
2023-04-27 00:38:37 -0400

    FIX: show all details in cart

--- models/Cart.js
+++ models/Cart.js
@@ -3,7 +3,15 @@ const connection = require("../database.js")
 class Cart {
 
     static showCart(custID,callback) {
-        const query = "SELECT outfit.*, cart.custID FROM outfit JOIN cart ON outfit.fitID=cart.fitID WHERE cart.custID=?";
+        const query =`SELECT outfit.fitID, outfit.designerEmail, bottoms.bottomName, tops.topName, shoes.shoeName, headwear.hwName, bags.bagName
+        FROM outfit
+        JOIN bottoms ON outfit.bottomID = bottoms.bottomID
+        JOIN tops ON outfit.topID = tops.topID
+        JOIN shoes ON outfit.shoeID = shoes.shoeID
+        JOIN headwear ON outfit.hwID = headwear.hwID
+        JOIN bags ON outfit.bagID = bags.bagID
+        JOIN cart ON outfit.fitID = cart.fitID
+        WHERE cart.custID = ?;`;
         connection.query(query, [custID], (error, result) => {
           if (error) {
             console.error(error);
@@ -25,7 +33,7 @@ class Cart {
     }
 
     static deleteFromCart(custID, fitID, callback) {
-      const query = "DELETE FROM cart WHERE custID = ? AND fitID = ?";
+      const query = "DELETE FROM cart WHERE custID = ? AND fitID = ? LIMIT 1";
       connection.query(query, [custID, fitID], (error, result) => {
         if (error) {
           console.error(error);
